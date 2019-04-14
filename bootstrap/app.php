b14a249ce5b19cccfcd84fe1ddeaa5e2c85ef045
GamaKF <gamalama050496@gmail.com>
2019-04-14 01:47:42 -0500

    Actualizacion de version para avance
    Se realizaron cambios para poder crear las migraciones y el control de versiones de modelos, se puede iniciar sesión, cerrar sesión y registrar correctamente. 
    PD: Se actualizo la version de laravel a 5.5 y se esta utilizando php 7+, esto para el correcto funcionamiento con MySQL 8+.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    devartsn\Http\Kernel::class
+    App\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    devartsn\Console\Kernel::class
+    App\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    devartsn\Exceptions\Handler::class
+    App\Exceptions\Handler::class
 );
 
 /*
--- database/migrations/2019_04_13_062920_create_teams_table.php
+++ database/migrations/2019_04_13_062920_create_teams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTeamsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('typeUser')->nullable();
-            $table->string('team1')->nullable();
-            $table->string('team2')->nullable();
-            $table->string('team3')->nullable();
-            $table->rememberToken();
+            $table->string('capacity');
+            $table->string('turnaround');
             $table->timestamps();
         });
     }
@@ -34,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teams');
     }
 }
